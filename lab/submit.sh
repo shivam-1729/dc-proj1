00ffeb43f725767b59d0091e1d756230e513f5b1
Yeaton, Joshua Levitt <jyeaton3@gatech.edu>
2025-01-23 08:30:50 -0500

    Removed mac specific files from submit.sh

--- lab/submit.sh
+++ lab/submit.sh
@@ -12,7 +12,7 @@ function verify() {
 		done
 }
 
-req_files=("src/.DS_Store" "src/dslabs/.DS_Store" "src/dslabs/clientserver/SimpleServer.java" "src/dslabs/clientserver/Timers.java" "src/dslabs/clientserver/Messages.java" "src/dslabs/clientserver/SimpleClient.java" "src/dslabs/kvstore/KVStore.java" "src/dslabs/atmostonce/AMOCommand.java" "src/dslabs/atmostonce/AMOResult.java" "src/dslabs/atmostonce/AMOApplication.java" "REPORT.md")
+req_files=("src/dslabs/clientserver/SimpleServer.java" "src/dslabs/clientserver/Timers.java" "src/dslabs/clientserver/Messages.java" "src/dslabs/clientserver/SimpleClient.java" "src/dslabs/kvstore/KVStore.java" "src/dslabs/atmostonce/AMOCommand.java" "src/dslabs/atmostonce/AMOResult.java" "src/dslabs/atmostonce/AMOApplication.java" "REPORT.md")
 verify "${req_files[@]}"
 if [[ $? -ne 0 ]]; then
     exit 1
@@ -20,7 +20,7 @@ fi
 
 if [ $# -eq 1 ]
 then
-	zip "${1}.zip" src/.DS_Store src/dslabs/.DS_Store src/dslabs/clientserver/SimpleServer.java src/dslabs/clientserver/Timers.java src/dslabs/clientserver/Messages.java src/dslabs/clientserver/SimpleClient.java src/dslabs/kvstore/KVStore.java src/dslabs/atmostonce/AMOCommand.java src/dslabs/atmostonce/AMOResult.java src/dslabs/atmostonce/AMOApplication.java REPORT.md
+	zip "${1}.zip" src/dslabs/clientserver/SimpleServer.java src/dslabs/clientserver/Timers.java src/dslabs/clientserver/Messages.java src/dslabs/clientserver/SimpleClient.java src/dslabs/kvstore/KVStore.java src/dslabs/atmostonce/AMOCommand.java src/dslabs/atmostonce/AMOResult.java src/dslabs/atmostonce/AMOApplication.java REPORT.md
 else
 	echo 'Please provide your GTID, eg ./submit.sh syi73'
 fi
